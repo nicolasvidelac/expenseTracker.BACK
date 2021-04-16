37690b726f361cb29b4d8dad315f206616edf10f
nicov <nicolasvidelac>
2021-04-16 17:56:03 -0300

    added regex for email validator

--- src/main/java/com/group/gastos/others/registration/EmailValidator.java
+++ src/main/java/com/group/gastos/others/registration/EmailValidator.java
@@ -3,12 +3,17 @@ package com.group.gastos.others.registration;
 import org.springframework.stereotype.Service;
 
 import java.util.function.Predicate;
+import java.util.regex.Pattern;
 
 @Service
 public class EmailValidator implements Predicate<String> {
+    private static final String regex =
+            "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+
     @Override
-    public boolean test(String s) {
-        //todo regex to validate email
-        return true;
+    public boolean test(String email) {
+        Pattern pattern = Pattern.compile(regex);
+
+        return pattern.matcher(email).matches();
     }
 }
