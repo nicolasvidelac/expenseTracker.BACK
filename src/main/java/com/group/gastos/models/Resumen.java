19cbddc6682044becf23b59a3daccd3ca538beef
nicov <nicolasvidelac>
2021-04-20 13:31:21 -0300

    resumen nombreMes en español

--- src/main/java/com/group/gastos/models/Resumen.java
+++ src/main/java/com/group/gastos/models/Resumen.java
@@ -1,5 +1,6 @@
 package com.group.gastos.models;
 
+import com.group.gastos.others.enums.MesesEnum;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -37,26 +38,9 @@ public class Resumen {
         this.valorDolar = valorDolar;
         this.fechaInicio = LocalDate.now().withDayOfMonth(1);
         this.fechaFin = LocalDate.now().withDayOfMonth(1).plusDays(LocalDate.now().lengthOfMonth() - 1);
-        this.nombreMes = upperCaseFirst(LocalDate.now().getMonth().name());
+        this.nombreMes = MesesEnum.valueOf(LocalDate.now().getMonth().name()).getLabel();
         this.usuario_id = usuario_id;
         this.estado = estado;
         this.items = new ArrayList<>();
-    }
-
-    private static String upperCaseFirst(String value) {
-
-        // Convert String to char array.
-        char[] array = value.toCharArray();
-
-        // Modify first element in array.
-        array[0] = Character.toUpperCase(array[0]);
-
-        //every other element to lower case
-        for(int i = 1; i<array.length ; i++){
-            array[i] = Character.toLowerCase(array[i]);
-        }
-
-        // Return string.
-        return new String(array);
     }
 }
---
+++ src/main/java/com/group/gastos/others/enums/MesesEnum.java
@@ -0,0 +1,25 @@
+package com.group.gastos.others.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum MesesEnum {
+    JANUARY("Enero"),
+    FEBRUARY("Febrero"),
+    MARCH("Marzo"),
+    APRIL("Abril"),
+    MAY("Mayo"),
+    JUNE("Junio"),
+    JULY("Julio"),
+    AUGUST("Agosto"),
+    SEPTEMBER("Septiembre"),
+    OCTOBER("Octubre"),
+    NOVEMBER("Noviembre"),
+    DECEMBER("Diciembre");
+
+    private final String label;
+
+    private MesesEnum(String label) {
+        this.label = label;
+    }
+}
